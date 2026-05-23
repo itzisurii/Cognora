edb0324620a2fe65744589e6f1245e776ff02006
itzisurii <itzIsurika08@gmail.com>
2026-05-23 11:38:23 +0530

    refactor: update UserRepository to use entity package for User model

--- cognora-backend/src/main/java/edu/iCET/repository/UserRepository.java
+++ cognora-backend/src/main/java/edu/iCET/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package edu.iCET.repository;
 
-import edu.iCET.model.User;
+import edu.iCET.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long>{
 }
