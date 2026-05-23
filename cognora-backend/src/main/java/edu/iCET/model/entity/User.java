f9857f8f99d0a2984d52d39d2fe63716de1222c0
itzisurii <itzIsurika08@gmail.com>
2026-05-23 11:38:09 +0530

    refactor: rename User class package and update table name annotation

--- cognora-backend/src/main/java/edu/iCET/model/entity/User.java
+++ cognora-backend/src/main/java/edu/iCET/model/entity/User.java
@@ -1,11 +1,9 @@
-package edu.iCET.model;
+package edu.iCET.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
