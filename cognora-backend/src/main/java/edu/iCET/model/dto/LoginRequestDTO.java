02ecaa58f85e5bb4ecbcdbdcd1bdba3f514e5a60
itzisurii <itzIsurika08@gmail.com>
2026-06-05 23:21:07 +0530

    feat: enhance LoginRequestDTO with Lombok annotations for cleaner code

--- cognora-backend/src/main/java/edu/iCET/model/dto/LoginRequestDTO.java
+++ cognora-backend/src/main/java/edu/iCET/model/dto/LoginRequestDTO.java
@@ -1,5 +1,14 @@
 package edu.iCET.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDTO {
     public String email;
     public String password;
